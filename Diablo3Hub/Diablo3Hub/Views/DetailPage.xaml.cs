e82bddad8e16fe1657ea77af99fd54114f4e3da3
DESKTOP-A933BID\kaki1 <kaki104@daum.net>
2017-05-07 11:42:59 +0900

    템플릿 프로젝트 생성후 처음 커밋 1. https://github.com/Windows-XAML/Template10/issues/1442 이 게시글에 있는 Visual Studio 2017용 Templete 10 설치 파일을 다운 받아서 설치 후 시작해야함

---
+++ Diablo3Hub/Diablo3Hub/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Diablo3Hub.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Diablo3Hub.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
---
+++ Diablo3Hub/Diablo3Hub/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Diablo3Hub.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace Diablo3Hub.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
