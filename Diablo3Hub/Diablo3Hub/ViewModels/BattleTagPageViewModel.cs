afdbf40fb7d53475e494bace41d4fdcab004efd4
Park Munchan <kaki104@hanmail.net>
2017-05-15 21:47:55 +0900

    BattleTagManagementDialog추가.. 아 언제 DB에 CRUD작업하나

--- Diablo3Hub/Diablo3Hub/ViewModels/BattleTagPageViewModel.cs
+++ Diablo3Hub/Diablo3Hub/ViewModels/BattleTagPageViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Windows.ApplicationModel;
 using Windows.UI.Xaml.Navigation;
 using Diablo3Hub.Commons;
+using Diablo3Hub.Controls;
 using Diablo3Hub.Models;
 using Template10.Mvvm;
 
@@ -38,29 +40,62 @@ namespace Diablo3Hub.ViewModels
             get => _battleTags;
             set => Set(ref _battleTags, value);
         }
+
         /// <summary>
-        /// 베틀테그 추가 커맨드
+        ///     베틀테그 추가 커맨드
         /// </summary>
         public ICommand AddBattleTagCommand { get; set; }
+
         /// <summary>
-        /// 초기화
+        ///     배틀테그 수정 커맨드
+        /// </summary>
+        public ICommand EditBattleTagCommand { get; set; }
+
+        /// <summary>
+        ///     배틀테그 삭제 커맨드
+        /// </summary>
+        public ICommand DeleteBattleTagCommand { get; set; }
+
+        /// <summary>
+        ///     선택하기 커맨드
+        /// </summary>
+        public ICommand SelectCommand { get; set; }
+
+        /// <summary>
+        ///     초기화
         /// </summary>
         private void Init()
         {
-            AddBattleTagCommand = new DelegateCommand(() =>
+            AddBattleTagCommand = new DelegateCommand(async () =>
             {
-                //등록 팝업 출력
-                //셈플
-                BattleTags = new List<BattleTag>
-                {
-                    new BattleTag
-                    {
-                        Server = GameConfigs.ServerKR,
-                        Locale = GameConfigs.LocaleKR,
-                        Tag = "SuperOwl-1417",
-                        Description = "카키입니다."
-                    }
-                };
+                ////등록 팝업 출력
+                ////셈플
+                //BattleTags = new List<BattleTag>
+                //{
+                //    new BattleTag
+                //    {
+                //        Server = GameConfigs.ServerKR,
+                //        Locale = GameConfigs.LocaleKR,
+                //        Tag = "SuperOwl-1417",
+                //        Description = "카키입니다."
+                //    },
+                //    new BattleTag
+                //    {
+                //        Server = GameConfigs.ServerKR,
+                //        Locale = GameConfigs.LocaleKR,
+                //        Tag = "SuperOwl-1417",
+                //        Description = "카키입니다."
+                //    },
+                //    new BattleTag
+                //    {
+                //        Server = GameConfigs.ServerKR,
+                //        Locale = GameConfigs.LocaleKR,
+                //        Tag = "SuperOwl-1417",
+                //        Description = "카키입니다."
+                //    }
+                //};
+                var battleTag = new BattleTagManagementDialog();
+                var result = await battleTag.ShowAsync();
             });
         }
 
