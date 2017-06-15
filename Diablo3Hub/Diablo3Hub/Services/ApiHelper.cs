39cf5df3f1762a3c9b8e6cb1cc422fd14ddf8f7a
Park Munchan <kaki104@hanmail.net>
2017-06-15 22:13:01 +0900

    ApiCache 모두 적용, 히어로 페이지에 카나이함과 패스브 스킬 추가

--- Diablo3Hub/Diablo3Hub/Services/ApiHelper.cs
+++ Diablo3Hub/Diablo3Hub/Services/ApiHelper.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Windows.Storage;
+using Windows.UI.Popups;
 using Diablo3Hub.Commons;
 using Diablo3Hub.Models;
 using Newtonsoft.Json;
-using System.IO;
-using Windows.UI.Popups;
 using Template10.Services.NetworkAvailableService;
 
 namespace Diablo3Hub.Services
@@ -19,6 +19,8 @@ namespace Diablo3Hub.Services
 
         private IList<KeyValuePair<string, string>> _apiKeys
             = new List<KeyValuePair<string, string>>();
+
+        private INetworkAvailableService _network;
 
         /// <summary>
         ///     인스턴스
@@ -40,12 +42,14 @@ namespace Diablo3Hub.Services
         {
             get { return !_apiKeys.Any() ? null : _apiKeys.FirstOrDefault(p => p.Key == "Secret").Value; }
         }
+
         /// <summary>
-        /// us, eu, tw, kr
+        ///     us, eu, tw, kr
         /// </summary>
         public string SelectedGameServer { get; set; }
+
         /// <summary>
-        /// Locale ko_KR
+        ///     Locale ko_KR
         /// </summary>
         public string SelectedLocale { get; set; }
 
@@ -62,48 +66,57 @@ namespace Diablo3Hub.Services
             {
                 var apiFile = await StorageFile.GetFileFromApplicationUriAsync(uri);
                 var content = await FileIO.ReadTextAsync(apiFile);
-                string[] stringSeparators = { "\r\n" };
+                string[] stringSeparators = {"\r\n"};
                 var lines = content.Split(stringSeparators, StringSplitOptions.None);
                 _apiKeys = (from kkk in lines
-                            where kkk.Length > 0  //Apikey 파일 편집시 new line이 들어가는 경우, outofbound Exception이 발생하여 이 부분을 처리하였습니다.
-                            let key = kkk.Split('=')
-                            select new KeyValuePair<string, string>(key[0], key[1])).ToList();
+                    where kkk.Length > 0 //Apikey 파일 편집시 new line이 들어가는 경우, outofbound Exception이 발생하여 이 부분을 처리하였습니다.
+                    let key = kkk.Split('=')
+                    select new KeyValuePair<string, string>(key[0], key[1])).ToList();
             }
             catch (FileNotFoundException)
             {
-                await new Windows.UI.Popups.MessageDialog("ApiKeys 파일을 찾지 못햇습니다. \n다시 확인해 주세요.").ShowAsync();
+                await new MessageDialog("ApiKeys 파일을 찾지 못햇습니다. \n다시 확인해 주세요.").ShowAsync();
                 return;
             }
             catch (Exception)
             {
-                await new Windows.UI.Popups.MessageDialog("ApiKeys 파일을 읽는 중에 문제가 발생하였습니다. \n 다시 확인해 주세요.").ShowAsync();
+                await new MessageDialog("ApiKeys 파일을 읽는 중에 문제가 발생하였습니다. \n 다시 확인해 주세요.").ShowAsync();
                 return;
             }
-            
+
             SelectedGameServer = GameConfigs.ServerKR;
             SelectedLocale = GameConfigs.LocaleKR;
         }
 
-        private INetworkAvailableService _network;
+        /// <summary>
+        ///     GetApiCacheData
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private static async Task<ApiCacheData> GetApiCacheDataAsync(string url)
+        {
+            var cache = await DBHelper.Instance.ApiCacheTable()
+                .Where(p => p.Url == url)
+                .FirstOrDefaultAsync();
+            var returnItem = cache?.CreateDT > DateTime.Now.AddMinutes(-5) ? cache : null;
+            if (returnItem == null && cache != null)
+                await DBHelper.Instance.DeleteAsync(cache);
+            return returnItem;
+        }
 
         /// <summary>
-        /// Get CareerProfile 
+        ///     Get CareerProfile
         /// </summary>
         /// <param name="battleTag"></param>
         /// <returns></returns>
         public async Task<CareerProfile> GetCareerProfileAsync(string battleTag)
         {
-            if (await _network.IsInternetAvailable() == false)
-            {
-                await CommonStaticHelper.ShowMessageBoxAsync("인터넷 연결이 필요합니다. 잠시후 다시 시도해 주시기 바랍니다.");
-                return null;
-            }
+            if (await CheckInternetAvailableAsync() == false) return null;
 
-            var url = $"https://{SelectedGameServer}.api.battle.net/d3/profile/{battleTag.Replace("#","-")}/?locale={SelectedLocale}&apikey={ApiKey}";
+            var url =
+                $"https://{SelectedGameServer}.api.battle.net/d3/profile/{battleTag.Replace("#", "-")}/?locale={SelectedLocale}&apikey={ApiKey}";
 
-            var cache = await DBHelper.Instance.ApiCacheTable()
-                .Where(p => p.Url == url)
-                .FirstOrDefaultAsync();
+            var cache = await GetApiCacheDataAsync(url);
             if (cache != null)
             {
                 var result = JsonConvert.DeserializeObject<CareerProfile>(cache.Content);
@@ -115,58 +128,95 @@ namespace Diablo3Hub.Services
                 var content = await hc.GetStringAsync(url);
                 var jsonResult = JsonConvert.DeserializeObject<CareerProfile>(content);
 
-                var item = new ApiCacheData
-                {
-                    Url = url,
-                    Content = content,
-                    CreateDT = DateTime.Now
-                };
-                var result = await DBHelper.Instance.InsertAsync(item);
+                await ApiDataInsertAsync(url, content);
                 return jsonResult;
             }
         }
+
         /// <summary>
-        /// 히어로 프로파일
+        ///     ApiData 추가
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        private static async Task ApiDataInsertAsync(string url, string content)
+        {
+            await DBHelper.Instance.InsertAsync(new ApiCacheData
+            {
+                Url = url,
+                Content = content,
+                CreateDT = DateTime.Now
+            });
+        }
+
+        /// <summary>
+        ///     인터넷 연결 상태 확인
+        /// </summary>
+        /// <returns></returns>
+        private async Task<bool> CheckInternetAvailableAsync()
+        {
+            if (await _network.IsInternetAvailable()) return true;
+            await CommonStaticHelper.ShowMessageBoxAsync("인터넷 연결이 필요합니다. 잠시후 다시 시도해 주시기 바랍니다.");
+            return false;
+        }
+
+        /// <summary>
+        ///     히어로 프로파일
         /// </summary>
         /// <param name="battleTag"></param>
         /// <param name="heroId"></param>
         /// <returns></returns>
         public async Task<HeroProfile> GetHeroProfileAsync(string battleTag, string heroId)
         {
-            if (await _network.IsInternetAvailable() == false)
+            if (await CheckInternetAvailableAsync() == false) return null;
+
+            var url =
+                $"https://{SelectedGameServer}.api.battle.net/d3/profile/{battleTag.Replace("#", "-")}/hero/{heroId}?locale={SelectedLocale}&apikey={ApiKey}";
+
+            var cache = await GetApiCacheDataAsync(url);
+            if (cache != null)
             {
-                await CommonStaticHelper.ShowMessageBoxAsync("인터넷 연결이 필요합니다. 잠시후 다시 시도해 주시기 바랍니다.");
-                return null;
+                var result = JsonConvert.DeserializeObject<HeroProfile>(cache.Content);
+                return result;
             }
 
             using (var hc = new HttpClient())
             {
-                var url = $"https://{SelectedGameServer}.api.battle.net/d3/profile/{battleTag.Replace("#", "-")}/hero/{heroId}?locale={SelectedLocale}&apikey={ApiKey}";
                 var content = await hc.GetStringAsync(url);
                 var jsonResult = JsonConvert.DeserializeObject<HeroProfile>(content);
+
+                await ApiDataInsertAsync(url, content);
                 return jsonResult;
             }
         }
+
         /// <summary>
-        /// 아이템 정보 조회
+        ///     아이템 정보 조회
         /// </summary>
         /// <param name="itemCode"></param>
         /// <returns></returns>
         public async Task<ItemDetail> GetItemDetailAsync(string itemCode)
         {
-            if (await _network.IsInternetAvailable() == false)
-            {
-                await CommonStaticHelper.ShowMessageBoxAsync("인터넷 연결이 필요합니다. 잠시후 다시 시도해 주시기 바랍니다.");
-                return null;
-            }
+            if (await CheckInternetAvailableAsync() == false) return null;
+
             var parts = itemCode.Split('/');
             if (parts.Length != 2 || parts.First() != "item") return null;
+            var url =
+                $"https://{SelectedGameServer}.api.battle.net/d3/data/item/{parts.Last()}?locale={SelectedLocale}&apikey={ApiKey}";
+
+            var cache = await GetApiCacheDataAsync(url);
+            if (cache != null)
+            {
+                var result = JsonConvert.DeserializeObject<ItemDetail>(cache.Content);
+                return result;
+            }
 
             using (var hc = new HttpClient())
             {
-                var url = $"https://{SelectedGameServer}.api.battle.net/d3/data/item/{parts.Last()}?locale={SelectedLocale}&apikey={ApiKey}";
                 var content = await hc.GetStringAsync(url);
                 var jsonResult = JsonConvert.DeserializeObject<ItemDetail>(content);
+
+                await ApiDataInsertAsync(url, content);
                 return jsonResult;
             }
         }
